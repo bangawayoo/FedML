b7c70ae67b4e3c607d3f6b5a0cb76314883b68d2
bangawayoo <bangawayoo@gmail.com>
2022-06-02 20:25:55 +0900

    edit on MR

--- fedml_api/distributed/poisoned_fedopt/FedOptClientManager.py
+++ fedml_api/distributed/poisoned_fedopt/FedOptClientManager.py
@@ -109,7 +109,7 @@ class FedOptClientManager(ClientManager):
             # Model replacement (How To Backdoor Federated Learning 2020, Bagdasaryan)
             use_MR_strategy = True
             if use_MR_strategy:
-                scale_factor = 3
+                scale_factor = 1
                 for k, v in weights.items():
                     weights[k] = scale_factor * v
 
